1e1cac0eab524c79886bc13f42ffe23931b12754
AnneFiger <annefiger@gmail.com>
2022-12-10 23:06:25 +0000

    Revert "modifying the date output format"
    This reverts commit f528ec57bd15d593407d9a2220c6082ac2201b61.
    
    Reverting commit

--- index.js
+++ index.js
@@ -35,8 +35,8 @@ app.get("/api/1451001600000", function (req, res) {
 
 // This below needs to be modified to be actually what's returned for an 
 // empty date parameter + unix timestamp
-app.get("/api/:date?", function (req, res, next) {
-  date = new Date(req.params.date).toString()
+app.get("/api/:date", function (req, res, next) {
+  date = new Date(req.params.date)
   next();
 }, function(req,res) {
   res.json({"unix":1451001600000, "utc": date}); 
