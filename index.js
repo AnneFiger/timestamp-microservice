880a7863049b072dd0fa8618eacd4395e8ba8f11
AnneFiger <annefiger@gmail.com>
2022-12-11 13:49:24 +0000

    Implementing basic function ie can grab a date or a unix timestamp and go from there. Does not deal with empty parameter or edge cases

--- index.js
+++ index.js
@@ -19,30 +19,31 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
-
-
-
 // A request to /api/1451001600000 should return { unix: 1451001600000, 
 // utc: "Fri, 25 Dec 2015 00:00:00 GMT" }
-app.get("/api/1451001600000", function (req, res) {
-  res.json({unix: 1451001600000, utc: "Fri, 25 Dec 2015 00:00:00 GMT"});
-});
+
+// app.get("/api/1451001600000", function (req, res) {
+//   res.json({unix: 1451001600000, utc: "Fri, 25 Dec 2015 00:00:00 GMT"});
+// });
 
 // This below needs to be modified to be actually what's returned for an 
-// empty date parameter 
-app.get("/api/:date", function (req, res, next) {
-  date = new Date(req.params.date)
-  dateAsUTCString = date.toUTCString()
-  unix = date[Symbol.toPrimitive]('number')
+// empty date parameter (separate function above this one?) - see what happens with an edge case ie request to 2015-02-31 -should return error
+app.get("/api/:date?", function (req, res, next) {
+  // date = new Date(req.params.date)
+  unixtest = Date.parse(req.params.date)
+  if (isNaN(unixtest)===false) { //if unixtest is valid meaning the date given in the request is a valid date
+    date = new Date(req.params.date)
+    dateAsUTCString = date.toUTCString()
+    unix = date[Symbol.toPrimitive]('number')
+  }else{
+    unix = req.params.date;
+    date = new Date(0);
+    date.setUTCMilliseconds(unix);
+    dateAsUTCString = date.toUTCString()
+  }
   next();
 }, function(req,res) {
-  res.json({"unix": unix, "utc": dateAsUTCString}); 
-  // "unix":1451001600000
+  res.json({unix: unix, utc: dateAsUTCString}); 
 });
 
 // listen for requests :)
